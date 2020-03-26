926e11f2e27cec50fd38461e2f4f4e82ade7e81a
Maciek Borzecki <maciek.borzecki@gmail.com>
2020-03-26 21:54:52 +0100

    cpu: add push/pop, cpu reset, addresses
    Signed-off-by: Maciek Borzecki <maciek.borzecki@gmail.com>

--- src/cpu.rs
+++ src/cpu.rs
@@ -21,10 +21,17 @@
 // SOFTWARE.
 
 mod CPU6510 {
+    const ADDR_STACK_START: u16 = 0x0100;
+    const ADDR_STACK_END: u16 = 0x01ff;
+
+    const ADDR_NMI_VECTOR: u16 = 0xfffa;
+    const ADDR_RESET_VECTOR: u16 = 0xfffc;
+    const ADDR_IRQ_VECTOR: u16 = 0xfffe;
+
     // CPU6510 is a model of MOS 6510 Microprocessor.
     // Based on http://archive.6502.org/datasheets/mos_6510_mpu.pdf
 
-    #[derive(Copy, Clone, Default)]
+    #[derive(Copy, Clone, Default, Debug)]
     struct ProgramCounter {
         // TODO: reduce to single u16?
         PCH: u8,
@@ -55,15 +62,44 @@ mod CPU6510 {
         }
     }
 
-    #[derive(Default)]
+    #[derive(Copy, Clone, Default, Debug, PartialEq)]
     struct StatusRegister {
         N: bool, // Negative
-        O: bool, // Overflow
+        V: bool, // Overflow
         B: bool, // BRK command
         D: bool, // Decimal model
         I: bool, // IRQ disable
         Z: bool, // Zero
         C: bool, // Carry
+    }
+
+    impl From<StatusRegister> for u8 {
+        fn from(sr: StatusRegister) -> u8 {
+            0 as u8 |
+            (sr.C as u8) << 0 |
+            (sr.Z as u8) << 1 |
+            (sr.I as u8) << 2 |
+            (sr.D as u8) << 3 |
+            (sr.B as u8) << 4 |
+            // reserved
+            (1 << 5) as u8 |
+            (sr.V as u8) << 6 |
+            (sr.N as u8) << 7
+        }
+    }
+
+    impl From<u8> for StatusRegister {
+        fn from(sr: u8) -> StatusRegister {
+            StatusRegister {
+                N: (sr & 1 << 7) != 0,
+                V: (sr & 1 << 6) != 0,
+                B: (sr & 1 << 4) != 0,
+                D: (sr & 1 << 3) != 0,
+                I: (sr & 1 << 2) != 0,
+                Z: (sr & 1 << 1) != 0,
+                C: (sr & 1) != 0,
+            }
+        }
     }
 
     #[derive(Default)]
@@ -72,7 +108,7 @@ mod CPU6510 {
         Y: u8,              // Y index register
         X: u8,              // X index register
         PC: ProgramCounter, // (PCH|PCL) program counter
-        S: u16,             // Stack pointer
+        S: u8,              // Stack pointer
         P: StatusRegister,  // Processor status register
     }
 
@@ -151,7 +187,7 @@ mod CPU6510 {
     pub struct CPU<'a> {
         r: Registers,
 
-        mem: &'a component::Memory<u16, u8>,
+        mem: &'a mut component::Memory<u16, u8>,
     }
 
     use crate::component;
@@ -172,9 +208,36 @@ mod CPU6510 {
             self.r.PC.inc();
             return insn;
         }
+
+        fn fetch_addr(&self, addr: u16) -> u16 {
+            let mut val = self.mem.read(addr) as u16;
+            val = val << 8;
+            val = val | (self.mem.read(addr+1) as u16);
+            return val;
+        }
+
+        fn reset(&mut self) {
+            self.r = Default::default();
+            self.r.PC.set(self.fetch_addr(ADDR_RESET_VECTOR));
+            self.r.S = 0xff;
+        }
+
+        // push onto the stack
+        fn push(&mut self, data: u8) {
+            // TODO: check limits
+            self.mem.write(ADDR_STACK_START + (self.r.S as u16), data);
+            self.r.S = self.r.S - 1;
+        }
+
+        // pop from the stack
+        fn pop(&mut self) -> u8 {
+            // TODO: check limits
+            self.r.S = self.r.S + 1;
+            self.mem.read(ADDR_STACK_START + (self.r.S as u16))
+        }
     }
 
-    pub fn new_with_memory(mem: &component::Memory<u16, u8>) -> CPU {
+    pub fn new_with_memory(mem: &mut component::Memory<u16, u8>) -> CPU {
         return CPU {
             r: Default::default(),
             mem: mem,
@@ -194,22 +257,65 @@ mod CPU6510 {
             }
         }
 
-        impl component::Memory<u16, u8> for [u8; 256] {
+        #[test]
+        fn sr_conversion_simple() {
+            let mut sr = StatusRegister {
+                C: true,
+                ..Default::default()
+            };
+            let v1 = u8::from(sr);
+            assert_eq!(v1, 1 << 5 | 0x1);
+            let sr2 = StatusRegister::from(v1);
+            assert_eq!(sr2, sr);
+        }
+
+        #[test]
+        fn sr_conversion_more() {
+            let mut sr = StatusRegister {
+                C: true,
+                N: true,
+                I: true,
+                ..Default::default()
+            };
+            let v1 = u8::from(sr);
+            assert_eq!(v1, 1 << 7 | 1 << 2 | 1 << 5 | 1);
+            let sr2 = StatusRegister::from(v1);
+            assert_eq!(sr2, sr);
+        }
+
+        impl component::Memory<u16, u8> for [u8; 65535] {
             fn read(&self, addr: u16) -> u8 {
                 return self[usize::from(addr)];
             }
-            fn write(&mut self, addr: u16, value: u8) {
-                self[usize::from(addr)] = value;
+            fn write(&mut self, addr: u16, data: u8) {
+                self[usize::from(addr)] = data;
             }
         }
 
         #[test]
         fn cpu_simple() {
-            let mut mem: [u8; 256] = [0; 256];
-            let mut cpu = new_with_memory(&mem);
+            let mut mem: [u8; 65535] = [0; 65535];
+            let mut cpu = new_with_memory(&mut mem);
             assert_eq!(u16::from(cpu.r.PC), 0);
             cpu.dispatch_one();
             assert_eq!(u16::from(cpu.r.PC), 1);
+        }
+
+        #[test]
+        fn cpu_reset() {
+            let mut mem: [u8; 65535] = [0; 65535];
+            let prog_start = ADDR_STACK_END + 1;
+            let (lo, hi) = ((prog_start & 0xff), (prog_start >> 8 & 0xff));
+
+            mem[ADDR_RESET_VECTOR as usize] = hi as u8;
+            mem[(ADDR_RESET_VECTOR+1) as usize] = lo as u8;
+
+            let mut cpu = new_with_memory(&mut mem);
+            cpu.reset();
+
+            assert_eq!(cpu.r.S, 0xff);
+            assert_eq!(u8::from(cpu.r.P), 1 << 5);
+            assert_eq!(u16::from(cpu.r.PC), ADDR_STACK_END + 1);
         }
     }
 }
